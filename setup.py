b22a00f526e3b415df75c8b97ecb3a862e7ce2dd
wrieg123 <will.r.rieger@gmail.com>
2021-03-30 14:26:44 -0700

    init initial shell

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup (
+    name= 'pytrader',
+    version= '0.9',
+    author = 'Will Rieger',
+    description= 'Python based futures and securities backtesting engine',
+    packages = find_packages(),
+    install_requires = ['numpy', 'pandas', 'matplotlib', 'sqlalchemy', 'mysql-connector', 'numba', 'tqdm', 'mysqlclient'],
+)
